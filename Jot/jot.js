fe54a88fbc93b83cf1e960690b795aa536697b46
Tyler Dixon <onyxqk@gmail.com>
2020-11-19 00:08:53 -0500

    tweaks

--- Jot/jot.js
+++ Jot/jot.js
@@ -2,12 +2,12 @@ var editableState = false;
 var contentItems = document.getElementsByClassName("contentItem");
 
 function toggleEditable() {
-    editableState = !editableState;
-    var i;
-    for (i = 0; i < contentItems.length; i++) {
-        contentItems[i].contentEditable = editableState;
-  } 
-  if(editableState) {
+  editableState = !editableState;
+  var i;
+  for (i = 0; i < contentItems.length; i++) {
+    contentItems[i].contentEditable = editableState;
+  }
+  if (editableState) {
     document.getElementById("editButton").textContent = "Done editing";
     document.getElementById("buttonGroup").classList.remove("hideContent");
   }
@@ -18,20 +18,20 @@ function toggleEditable() {
 }
 
 function bold() {
-    document.execCommand("bold");
+  document.execCommand("bold");
 }
 
 
 function italicize() {
-    document.execCommand("italic");
+  document.execCommand("italic");
 }
 
 function keyHandler(e) {
-  if (e.ctrlKey &&  e.key === "b" || e.metaKey && e.key ==="b") {
+  if (e.ctrlKey && e.key === "b" || e.metaKey && e.key === "b") {
     e.preventDefault();
     bold();
   }
-  if (e.ctrlKey &&  e.key === "i" || e.metaKey && e.key ==="i") {
+  if (e.ctrlKey && e.key === "i" || e.metaKey && e.key === "i") {
     e.preventDefault();
     italicize();
   }
